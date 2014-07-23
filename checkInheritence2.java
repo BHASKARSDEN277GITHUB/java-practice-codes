b796d08e314db5d4ad6b64284248ebd2525897e5
BHASKAR <bhaskar@bhaskar-Dell-System-XPS-L502X.(none)>
2014-07-24 00:07:18 +0530

    changes to Inheritence2.java

--- checkInheritence2.java
+++ checkInheritence2.java
@@ -1,12 +1,10 @@
-
-
 public class checkInheritence2{
 	
 	static class sub1{
 	}
-	static class sub2 extends sub1 {
+	static class sub2 extends private sub1 { // you cant do this bitch ..
 	}
-	static class sub3 extends sub2,sub1{
+	static class sub3 extends sub2,sub1{ // neither this  no multiple inheritence allowed ..
 	}
 	
 	public static void main(String[] args)
