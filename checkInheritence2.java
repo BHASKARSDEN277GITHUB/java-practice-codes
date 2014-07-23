a365a64be27f7a28ed8c18b443296968b05ae786
BHASKAR <bhaskar@bhaskar-Dell-System-XPS-L502X.(none)>
2014-07-24 00:00:16 +0530

    added another code (Inheritence)

---
+++ checkInheritence2.java
@@ -0,0 +1,15 @@
+
+
+public class checkInheritence2{
+	
+	static class sub1{
+	}
+	static class sub2 extends sub1 {
+	}
+	static class sub3 extends sub2,sub1{
+	}
+	
+	public static void main(String[] args)
+	{
+	}
+}
